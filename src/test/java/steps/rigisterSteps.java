0b92f7f8f3cfc55dd660eca00a32e390c9711484
mstira1 <137098737+mstira1@users.noreply.github.com>
2025-04-23 13:59:52 +0200

    @FindBy(css="") 	private WebElement webElement; commit push11

--- src/test/java/steps/rigisterSteps.java
+++ src/test/java/steps/rigisterSteps.java
@@ -3,6 +3,7 @@ package steps;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -17,7 +18,12 @@ import java.time.Duration;
 
 public class rigisterSteps {
 
+
     WebDriver driver;
+    private WebElement waitForElement(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
 
     @Given("User navigates to the registration page")
@@ -28,5 +34,30 @@ public class rigisterSteps {
         driver.manage().window().maximize();
         driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().refresh();
+    }
+
+    @When("användaren fyller i alla obligatoriska fält korrekt")
+    public void anvandarenFyllerIAllaObligatoriskaFaltKorrekt() {
+
+        driver.findElement(By.id("member_firstname")).sendKeys("alio");
+
+
+    }
+
+
+
+    @And("lösenorden matchar")
+    public void losenordenMatchar() {
+
+    }
+
+    @And("användaren godkänner terms and conditions")
+    public void anvandarenGodkannerTermsAndConditions() {
+
+    }
+
+    @Then("ett konto skapas och en bekräftelse visas")
+    public void ettKontoSkapasOchEnBekraftelseVisas() {
     }
 }
